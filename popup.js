225d4ebab0ad645993486bcb7c28b77b9c370cde
HB <84929878+Deadzu@users.noreply.github.com>
2023-10-31 10:57:32 +0100

    Kode

---
+++ popup.js
@@ -0,0 +1,7 @@
+<script>
+        // Add a click event listener to the button
+        document.getElementById('complimentButton').addEventListener('click', function() {
+            // Show an alert when the button is clicked
+            alert('You are amazing!'); // You can change the message as needed
+        });
+    </script>
