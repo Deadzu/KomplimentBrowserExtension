c6c7edf2ad89e3a7b8fa908bb725fa3b7680ef66
HB <84929878+Deadzu@users.noreply.github.com>
2023-11-13 12:51:51 +0100

    Ændringer

--- popup.js
+++ popup.js
@@ -1,7 +1,48 @@
-<script>
-        // Add a click event listener to the button
-        document.getElementById('complimentButton').addEventListener('click', function() {
-            // Show an alert when the button is clicked
-            alert('You are amazing!'); // You can change the message as needed
-        });
-    </script>
+// Array of general compliments
+const generalCompliments = [
+    "You're amazing!",
+    "You're a ray of sunshine!",
+    "You're exceptionally talented!",
+    "You're one of a kind!",
+    "You make the world a better place!",
+    "You're simply the best!",
+    "You're a true inspiration!",
+    "You're so kind-hearted!",
+  ];
+  
+  // Array of compliments about looks
+  const looksCompliments = [
+    "You look stunning!",
+    "Your style is impeccable!",
+    "You have a great sense of fashion!",
+    "Your smile is contagious!",
+    "You're absolutely beautiful!",
+    "You're handsome!",
+    "You radiate confidence!",
+    "You have an attractive personality!",
+  ];
+  
+  // Function to generate a random compliment
+  function generateRandomCompliment(complimentArray) {
+    const randomIndex = Math.floor(Math.random() * complimentArray.length);
+    return complimentArray[randomIndex];
+  }
+  
+  // Function to display a random general compliment
+  function displayGeneralCompliment() {
+    const compliment = generateRandomCompliment(generalCompliments);
+    document.getElementById("complimentDisplay").textContent = compliment;
+  }
+  
+  // Function to display a random looks compliment
+  function displayLooksCompliment() {
+    const compliment = generateRandomCompliment(looksCompliments);
+    document.getElementById("complimentDisplay").textContent = compliment;
+  }
+  
+  // Attach event listeners to the buttons
+  document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("generalComplimentButton").addEventListener("click", displayGeneralCompliment);
+    document.getElementById("looksComplimentButton").addEventListener("click", displayLooksCompliment);
+  });
+  
